444160d599e1fd6d2e0dab2a555935c2fde888f4
jefreyzavala1 <jefreyzavala1@gmail.com>
2023-05-26 17:35:26 -0400

    Magic 8 Ball express application create.

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
   res.send(`<h2>Your tip amount is : ${totalTip}</h2>`)
 })
 
+app.get('/magic/:question', (req, res) => {
+  const magicEightBallAnswers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it', 'As I see it yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', "Don't count on it", 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
+
+  const length = magicEightBallAnswers.length
+
+  res.send(`<h1> You have asked the magic ball : ${req.params.question} </h1>
+    <h1>The magic 8 ball says: ${magicEightBallAnswers[Math.floor(Math.random() * length)]}</h1>`)
+})
+
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`)
 })
